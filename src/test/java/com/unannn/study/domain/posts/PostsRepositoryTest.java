7ffa96acb297630c84a1dec237e116e3c686f49e
이윤환 <woooia1@gmail.com>
2022-02-28 13:50:11 +0900

    Auditing Entity 테스트 추가

--- src/test/java/com/unannn/study/domain/posts/PostsRepositoryTest.java
+++ src/test/java/com/unannn/study/domain/posts/PostsRepositoryTest.java
@@ -8,11 +8,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest/*(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)*/
 public class PostsRepositoryTest {
 
     @Autowired
@@ -44,4 +45,24 @@ public class PostsRepositoryTest {
         Assertions.assertThat(posts.getContent()).isEqualTo(content);
     }
 
+    @Test
+    public void BaseTimeEntity_등록() {
+        //given
+        LocalDateTime now = LocalDateTime.of(2019, 6, 4, 0, 0, 0);
+        postsRepository.save(Posts.builder()
+                .title("title")
+                .content(("content"))
+                .author("author")
+                .build());
+
+        //when
+        List<Posts> postsList = postsRepository.findAll();
+
+        //then
+        Posts posts = postsList.get(0);
+        System.out.println(">>>>>>>>>>>>>> createDate="+posts.getCreateDate() + ", modifiedDate="+ posts.getModifiedDate());
+
+        Assertions.assertThat(posts.getCreateDate()).isAfter(now);
+        Assertions.assertThat(posts.getModifiedDate()).isAfter(now);
+    }
 }
