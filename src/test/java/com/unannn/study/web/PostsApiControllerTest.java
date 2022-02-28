45a553f561786a54ead5c5567d1da059706a5b27
이윤환 <woooia1@gmail.com>
2022-02-28 13:45:44 +0900

    수정, 조회 단위 테스트 추가

--- src/test/java/com/unannn/study/web/PostsApiControllerTest.java
+++ src/test/java/com/unannn/study/web/PostsApiControllerTest.java
@@ -3,7 +3,7 @@ package com.unannn.study.web;
 import com.unannn.study.domain.posts.Posts;
 import com.unannn.study.domain.posts.PostsRepository;
 import com.unannn.study.web.dto.PostsSaveRequestDto;
-import org.assertj.core.api.Assertions;
+import com.unannn.study.web.dto.PostsUpdateRequestDto;
 import org.junit.After;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -11,11 +11,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.Assert.*;
@@ -39,7 +42,7 @@ public class PostsApiControllerTest {
     }
 
     @Test
-    public void ports_등록된다() throws Exception {
+    public void ports_등록() throws Exception {
         //given
         String title = "title";
         String content = "content";
@@ -62,4 +65,88 @@ public class PostsApiControllerTest {
         assertThat(all.get(0).getTitle()).isEqualTo(title );
         assertThat(all.get(0).getContent()).isEqualTo(content);
     }
+
+    @Test
+    public void posts_수정() throws Exception {
+        //given
+
+        Posts posts = Posts.builder()
+                .title("title")
+                .content("content")
+                .author("author")
+                .build();
+        Posts savedPosts = postsRepository.save(posts);
+
+        Long updateId =savedPosts.getId();
+        String title = "title2";
+        String content = "content2";
+
+        PostsUpdateRequestDto requestDto = PostsUpdateRequestDto.builder()
+                .title(title)
+                .content(content)
+                .build();
+
+        String url = "http://localhost:" + port + "/api/v1/posts/" + updateId;
+
+        HttpEntity<PostsUpdateRequestDto> requestDtoHttpEntity = new HttpEntity<>(requestDto);
+
+        //when
+        ResponseEntity<Long> responseEntity = restTemplate.exchange(url, HttpMethod.PUT, requestDtoHttpEntity, Long.class);
+
+        //then
+        System.out.println(requestDtoHttpEntity);
+        System.out.println(responseEntity);
+
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(responseEntity.getBody()).isGreaterThan(0L);
+
+        List<Posts> all = postsRepository.findAll();
+        assertThat(all.get(0).getTitle()).isEqualTo(title);
+        assertThat(all.get(0).getContent()).isEqualTo(content);
+    }
+
+    @Test
+    public void 아이디로_조회() throws Exception {
+
+        Posts posts = Posts.builder()
+                .title("title")
+                .author("author")
+                .content("content")
+                .build();
+
+        //given
+        Posts savedPosts = postsRepository.save(posts);
+
+        //when
+        Posts findPosts = postsRepository.findById(savedPosts.getId()).orElseThrow(() ->
+                new IllegalArgumentException("게시글을 찾을 수 없습니다."));
+
+        //then
+        assertThat(savedPosts.getId()).isEqualTo(findPosts.getId());
+        assertThat(savedPosts.getTitle()).isEqualTo(findPosts.getTitle());
+        assertThat(savedPosts.getContent()).isEqualTo(findPosts.getContent());
+        assertThat(savedPosts.getAuthor()).isEqualTo(findPosts.getAuthor());
+    }
+
+    @Test
+    public void posts_삭제() throws Exception {
+        //given
+        Posts savedPosts = postsRepository.save(Posts.builder()
+                .title("title")
+                .author("author")
+                .content("content")
+                .build());
+
+        String url = "http://localhost:" + port + "/api/v1/posts/" + savedPosts.getId();
+        HttpEntity<Long> requestDtoHttpEntity = new HttpEntity<>(savedPosts.getId());
+
+        //when
+        restTemplate.delete(url, Long.class);
+
+        //then
+        List<Posts> all = postsRepository.findAll();
+
+        assertThat(all.size()).isEqualTo(0);
+    }
+
 }
